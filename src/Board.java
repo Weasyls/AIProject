2c631eac99d7318b967cd1f38e491d0d8a16c860
Emran <emirhanakiturk@gmail.com>
2022-11-30 13:07:56 +0300

    State

--- src/Board.java
+++ src/Board.java
@@ -56,6 +56,9 @@ public class Board {
             throw e;
         }
         
+        
     }
 
+    
+        
 }
---
+++ src/State.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Administrator
+ */
+public class State {
+    
+    enum Player{P1, P2, AI};
+    
+    
+    public boolean gameOverState(){
+        
+        
+        
+        return false;
+    }
+}
